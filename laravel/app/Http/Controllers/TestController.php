d5be79ae4ab0995a78f50e8d7e56c01599eeebd1
shechunxiao <282074579@qq.com>
2019-09-05 23:04:10 +0800

    配置文件

---
+++ laravel/app/Http/Controllers/TestController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Test;
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    //
+    public function index(Test $test){
+
+    }
+}
